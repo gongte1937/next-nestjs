01eff2a6bdfe8c561e6f3c92aa9ffc303ab2869e
Terry <gongte1937@gmail.com>
2025-04-05 20:39:55 +1030

    update the user entity

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get, Post } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  public getUsers() {
+  @Get(':id')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log({ id });
+    console.log(typeof id);
     return 'Hello, users!';
   }
 
   @Post()
-  public createUser() {
+  public createUser(@Body() request: Request) {
+    console.log({ request });
     return 'User created!';
   }
 }
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -8,27 +8,19 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const router = useRouter();
 
-  // const handleLogin = () => {
-  //   // Mock login logic
-  //   if (username === "admin" && password === "password") {
-  //     localStorage.setItem("isLoggedIn", "true");
-  //     router.push("/");
-  //   } else {
-  //     alert("Invalid credentials");
-  //   }
-  // };
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5001/user", {
+      const response = await axios.post("http://localhost:5001/users", {
         username,
         password,
       });
+      console.log(" response:", response.data);
       const { access_token } = response.data;
       localStorage.setItem("token", access_token);
       router.push("/");
     } catch (err) {
-      console.log("登录失败：用户名或密码错误", err);
+      console.log("login in failed:", err);
     }
   };
 
@@ -51,7 +43,7 @@ export default function Login() {
       />
       <button
         onClick={handleLogin}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
+        className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
       >
         Login
       </button>
